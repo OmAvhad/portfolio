9fa739d7890af55348a6165d96de84521d7b3833
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2023-07-06 22:28:54 +0530

    added under development banner

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,15 @@
 import Navbar from "./navbar"
+import TopBar from "@/components/topBar"
 
 const Layout = ({ children }) => {
     return (
-        <div>
-            <div className="flex flex-col min-h-[calc(110vh-100px)] w-screen scroll-smooth px-10 py-8">
+        <>
+            <TopBar/>
+            <div className="flex flex-col w-screen scroll-smooth px-10 py-8">
                 <Navbar/>
                 {children}
             </div>
-        </div>
+        </>
     )
 }
 
---
+++ src/components/topBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopBar() {
+    return (
+        <div className='flex flex-row w-screen bg-gray-700 text-white items-center justify-center'>
+            Under Development 🏗️
+        </div>
+    )
+}
+
+export default TopBar
