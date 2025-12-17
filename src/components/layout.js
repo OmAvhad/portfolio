db862957516779ec5c8227e8ac99c00b868bda92
Om Avhad <omavhad22@gmail.com>
2025-12-17 14:35:23 +0530

    fixed treks page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,15 @@
 import Footer from "./Footer"
 import Sidebar from "./Sidebar"
+import { useRouter } from "next/router"
 
 const Layout = ({ children }) => {
+    const router = useRouter()
+    const showSidebar = router.pathname === '/'
+
     return (
         <div className="flex min-h-screen bg-white text-black">
-            <Sidebar />
-            <main className="flex-1 lg:pl-32 px-5 md:px-10 max-w-5xl mx-auto w-full">
+            {showSidebar && <Sidebar />}
+            <main className={`flex-1 ${showSidebar ? 'lg:pl-32' : ''} px-5 md:px-10 max-w-6xl mx-auto w-full`}>
                 {children}
                 <Footer/>
             </main>
